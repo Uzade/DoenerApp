497513891d90a4a2cd4d2188b1773e44a0435fc8
Joschka <joschka@startmail.com>
2023-03-19 09:07:47 +0100

    add endpoint to send orders to

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import { Database } from "sqlite3"
 import { PromissingSQLite3 } from "promissing-sqlite3/lib"
 import login from "./functions/login"
+import recieveOrders from "./functions/recieveOrders"
 
 var app = express()
 
@@ -17,6 +18,7 @@ const db = new PromissingSQLite3(new Database("./Database.db"))
 db.execFile("./sql/NewTable.sql")
 
 login(app, db)
+recieveOrders(app, db)
 
 app.listen(8003, 
     () => console.log("App live on http://localhost:8003")
---
+++ backend/sql/getUser.sql
@@ -0,0 +1,1 @@
+SELECT apiKey FROM users WHERE uid =?
