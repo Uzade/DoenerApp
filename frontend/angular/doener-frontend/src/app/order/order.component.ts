a710f0a6f7adddfd6645c51c4b52f382c9b4683b
Adrian John <adrianjohn6543@gmail.com>
2023-03-27 22:15:38 +0200

    refactor moved two functions  in the Service where they can be used globally / generic now

--- frontend/angular/doener-frontend/src/app/order/order.component.ts
+++ frontend/angular/doener-frontend/src/app/order/order.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
-import { HttpClient} from '@angular/common/http';
 import { StorageService } from '../storage.service';
-import { UUID } from '../uuid';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -13,28 +11,27 @@ export class OrderComponent implements OnInit{
   constructor(
     private route:ActivatedRoute
     ,private router:Router
-    ,private http: HttpClient
     ,private storage: StorageService
   ) { }
 
   code : string = "";
-  url : string = "http://localhost:8003/redirect?code=";
 
   ngOnInit(): void  {
-
     this.route.queryParams
       .subscribe(params => {
         this.code = params['code'];
-        //redirect the user to the login page if the login ether didnt work or he just straight-up wnt to order
-        if(this.code == undefined){
-          this.router.navigate(['/login']);
-        }
+        this.redirect();
       }
     ); 
-    //TODO idk warum this.code nd undefined ist aber funktioniert deshalb :shrug:
-    this.http.get<UUID>(this.url+this.code).subscribe(data =>{
-      this.storage.uuid = data;
-    })    
+    this.storage.getUUID(this.code);
+  }
 
+  /**
+   * redirect the user to the login page if the login ether didnt work or he just straight-up went to order
+   */
+  public redirect(){
+    if(this.code == undefined){
+      this.router.navigate(['/login']);
+    }
   }
 }
--- frontend/angular/doener-frontend/src/app/storage.service.ts
+++ frontend/angular/doener-frontend/src/app/storage.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Order } from './order';
 import { UUID } from './uuid';
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
   uuid : UUID | undefined;
-  constructor() { }
+  url : string = "http://localhost:8003/";
+  constructor(private http: HttpClient) { }
+
+
+  public sendOrder(order: Order) {
+    //TODO joschka fragen wie
+  }  
+  /**
+   * //TODO idk warum this.code nd undefined ist aber funktioniert deshalb :shrug:
+   * @param code the code given by the url parameter
+   */
+  public getUUID(code:string){
+    this.http.get<UUID>(this.url+"redirect?code="+code).subscribe(data =>{
+      this.uuid = data;
+    }) 
+  }
+  public getOrders():Order[]{
+    //TODO joschka fragen wie
+    return new Array
+  }
+
+    
 }
