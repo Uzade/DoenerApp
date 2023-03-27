57426ee8aedc6e0ba5f1cb019c239de5290429b2
Adrian John <adrianjohn6543@gmail.com>
2023-03-27 19:28:49 +0200

    add some basic routing from login to orders and back

--- frontend/angular/doener-frontend/src/app/app-routing.module.ts
+++ frontend/angular/doener-frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AllordersComponent } from './allorders/allorders.component';
 import { LoginComponent } from './login/login.component';
 import { OrderComponent } from './order/order.component';
+
 const routes: Routes = [
   {path: 'login',component: LoginComponent},
   {path: 'order',component: OrderComponent},
--- frontend/angular/doener-frontend/src/app/order/order.component.ts
+++ frontend/angular/doener-frontend/src/app/order/order.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import axios from "axios";
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent {
+  
 
 }
