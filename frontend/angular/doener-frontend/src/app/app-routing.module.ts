e85cf670f56e51da1eb741dfc1959b9fa48d9fac
Adrian John <adrianjohn6543@gmail.com>
2023-03-27 18:58:35 +0200

    add modules for routing

--- frontend/angular/doener-frontend/src/app/app-routing.module.ts
+++ frontend/angular/doener-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AllordersComponent } from './allorders/allorders.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+const routes: Routes = [
+  {path: 'login',component: LoginComponent},
+  {path: 'order',component: OrderComponent},
+  {path: 'allorders',component: AllordersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/angular/doener-frontend/src/app/app.module.ts
+++ frontend/angular/doener-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { AllordersComponent } from './allorders/allorders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OrderComponent,
+    AllordersComponent
   ],
   imports: [
     BrowserModule,
