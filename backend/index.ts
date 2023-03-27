fbc9fd0af995e64bf6a58763b93351678478f9de
Joschka <joschka@startmail.com>
2023-03-27 20:40:55 +0200

    delete orders at end of day

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ import { PromissingSQLite3 } from "promissing-sqlite3/lib"
 import login from "./functions/login"
 import recieveOrders from "./functions/recieveOrders"
 import sendAllOrders from "./functions/sendAllOrders"
+import schedule from 'node-schedule';
 
 var app = express()
 
@@ -17,6 +18,11 @@ app.use(express.json())
 const db = new PromissingSQLite3(new Database("./Database.db"))
 
 db.execFile("./sql/NewTable.sql")
+
+schedule.scheduleJob({hour: 0, minute: 0}, () => {
+    console.log('All Doeners have been deleted');
+    db.execFile("./sql/clearOrders.sql")
+  });
 
 login(app, db)
 recieveOrders(app, db)
