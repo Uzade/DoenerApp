9234c0fd1ddde54fbf3fc529f9ea8bf6520cb179
Adrian John <adrianjohn6543@gmail.com>
2023-03-27 21:39:24 +0200

    add a bunch of code idk what it does read doc

--- frontend/angular/doener-frontend/src/app/order/order.component.ts
+++ frontend/angular/doener-frontend/src/app/order/order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { HttpClient} from '@angular/common/http';
+import { StorageService } from '../storage.service';
+import { UUID } from '../uuid';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -11,30 +13,28 @@ export class OrderComponent implements OnInit{
   constructor(
     private route:ActivatedRoute
     ,private router:Router
-    ,private http: HttpClient) { }
+    ,private http: HttpClient
+    ,private storage: StorageService
+  ) { }
 
   code : string = "";
-  url : string = "http://localhost:5173/redirect?code=";
-  uuid : UUID | undefined;
-  ngOnInit(): void {
+  url : string = "http://localhost:8003/redirect?code=";
+
+  ngOnInit(): void  {
+
     this.route.queryParams
       .subscribe(params => {
-
         this.code = params['code'];
-
+        //redirect the user to the login page if the login ether didnt work or he just straight-up wnt to order
         if(this.code == undefined){
           this.router.navigate(['/login']);
         }
       }
     ); 
+    //TODO idk warum this.code nd undefined ist aber funktioniert deshalb :shrug:
     this.http.get<UUID>(this.url+this.code).subscribe(data =>{
-      this.uuid = data;
-      console.log(this.uuid)
-    })
-  }
-}
+      this.storage.uuid = data;
+    })    
 
-interface UUID {
-  apikey : string;
-  uid : string;
+  }
 }
