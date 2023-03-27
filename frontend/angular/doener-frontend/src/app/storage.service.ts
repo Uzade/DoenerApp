2c1c7fbecbc8d984f2090c859b164636901a391d
Adrian John <adrianjohn6543@gmail.com>
2023-03-27 21:37:39 +0200

    add storage where later everything will be stored

--- frontend/angular/doener-frontend/src/app/storage.service.ts
+++ frontend/angular/doener-frontend/src/app/storage.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-
+import { UUID } from './uuid';
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
-  
+  uuid : UUID | undefined;
   constructor() { }
 }
