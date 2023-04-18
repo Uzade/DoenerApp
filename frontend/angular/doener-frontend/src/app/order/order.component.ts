9cbd6f11283ed2b4c7c14b2cbd0dddf76fb97586
Adrian John <adrianjohn6543@gmail.com>
2023-04-18 19:25:05 +0200

    add Better FeedBack to order Component

--- frontend/angular/doener-frontend/src/app/order/order.component.ts
+++ frontend/angular/doener-frontend/src/app/order/order.component.ts
@@ -13,10 +13,12 @@ export class OrderComponent implements OnInit{
   constructor(
     private route:ActivatedRoute,
     private storage: StorageService
-  ) { }
+  ) {}
 
   code! : string;
-  submitFailed : boolean = false;
+  submitted! : boolean;
+  feedBack! : String;
+
   ngOnInit(): void  {
     this.route.queryParams
       .subscribe(params => {
@@ -25,7 +27,6 @@ export class OrderComponent implements OnInit{
       }
     ); 
     this.storage.getUUID(this.code);
-    this.submitFailed = false;
   }
 
   /**
@@ -36,12 +37,12 @@ export class OrderComponent implements OnInit{
       this.storage.gotoLogin();
     }
   }
-
+  
   submit(typ:HTMLInputElement,options:HTMLInputElement,message:HTMLInputElement){
-    this.submitFailed = false;
-    if(typ.value == "" || options.value == ""){
-      this.submitFailed = true;
-    }
+    
+    this.initVal();
+    if(!this.validateTyp(typ.value) || !this.validateOptions(options.value))return;
+
     let order : Order = {
       message : message.value,
       option : options.value,
@@ -49,6 +50,25 @@ export class OrderComponent implements OnInit{
       uid : this.storage.uuid!.uid,
       apiKey : this.storage.uuid!.apiKey
     }
+
     this.storage.sendOrder(order)
+  }
+  private initVal() {
+    this.submitted = true;
+    this.feedBack = "Worked";
+  }
+  private validateTyp(typ:String) : boolean{
+    if(typ == ""){
+      this.feedBack = "DoenerType is missing";
+      return false;
+    }
+    return true;
+  }
+  private validateOptions(options : String): boolean{
+    if(options == ""){
+      this.feedBack = "Options are missing";
+      return false;
+    }
+    return true;
   }
 }
